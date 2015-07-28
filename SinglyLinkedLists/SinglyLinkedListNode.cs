bd6139c36de8993d06ff376f2e7fa8c8f07f1bd0
johnwquarles <john.w.quarles@gmail.com>
2015-07-27 21:14:23 -0500

    have 10 tests passing

--- SinglyLinkedLists/SinglyLinkedListNode.cs
+++ SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -15,14 +15,20 @@ namespace SinglyLinkedLists
         private SinglyLinkedListNode next;
         public SinglyLinkedListNode Next
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return next; }
+            // can use keyword value for setter.
+            set {
+                if (value == this) { throw new ArgumentException(); }
+                this.next = value;
+            }
         }
 
-        private string value;
+        private string value; // same as this.value
+        // setters and getters are for things outside of the class to access/set data in the class.
+        // don't need to use them from within the class.
         public string Value 
         {
-            get { throw new NotImplementedException(); }
+            get { return value; }
         }
 
         public static bool operator <(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
@@ -39,13 +45,17 @@ namespace SinglyLinkedLists
 
         public SinglyLinkedListNode(string value)
         {
-            throw new NotImplementedException();
+            // don't *need* to write "this"; linter may throw errors. But it's ok to.
+            this.value = value;
+            // undeclared data memebers default to null, but we need a getter to "see" their null-ness.
+            //this.next = null;
 
             // Used by the visualizer:
             allNodes.Add(this);
         }
 
         // READ: http://msdn.microsoft.com/en-us/library/system.icomparable.compareto.aspx
+        // below is where we'll establish what equality even means in the case of these nodes.
         public int CompareTo(Object obj)
         {
             throw new NotImplementedException();
@@ -53,7 +63,12 @@ namespace SinglyLinkedLists
 
         public bool IsLast()
         {
-            throw new NotImplementedException();
+            return this.next == null;
+        }
+
+        public override string ToString()
+        {
+            return this.value;
         }
     }
 }
